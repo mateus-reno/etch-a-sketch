08ddf12d1971270d9d0e365ff8153c01d0aa0703
Mateus Kratz <mateuskratz@gmail.com>
2020-04-26 16:33:00 +0800

    Missing only number 3 and 5

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,42 @@
 //Event listeners Create Grid
 let btnCreateGrid = document.querySelector('#btnCreateGrid')
-btnCreateGrid.onclick = () => createGrid(askForUserInput(), askForUserInput())
+btnCreateGrid.onclick = () => createGrid(askForUserInputX(), askForUserInputY())
 
-//Event listeners Clear Grid
-let btnClearGrid = document.querySelector('#btnClearGrid')
-btnClearGrid.onclick = () => createGrid(askForUserInput(), askForUserInput())
 
-// Function to ask for user input on grid size
-function askForUserInput(){
-    let gridSize = window.prompt("What grid size do you want?").toLowerCase()
-    return gridSize
+let xAxis = 0
+let yAxis = 0
+
+// Function to ask for user input on grid size xAxis
+function askForUserInputX(){
+    let xAxis = window.prompt("What grid size do you want on the xAxis?").toLowerCase()
+    return xAxis
+}
+
+// Function to ask for user input on grid size yAxis
+function askForUserInputY(){
+    let yAxis = window.prompt("What grid size do you want on the yAxis?").toLowerCase()
+    return yAxis
 }
 
 // Function to create the grid
 function createGrid(xAxis, yAxis){
-    for (i = 1; i <= xAxis * yAxis; i++) {
         let sketchpad = document.querySelector('#container')
-        let column = xAxis + "fr";
-        sketchpad.setAttribute("grid-template-columns", column) 
+        let column = "repeat(auto-fill," + 960 / xAxis + "px)";
+        sketchpad.setAttribute("style", "grid-template-columns:" + column) 
+        console.log(sketchpad)
+    for (i = 1; i <= xAxis * yAxis; i++) {
         let newDiv = document.createElement('div')
-        newDiv.setAttribute('style', 'flex:1; margin: 1px; background: blue'); 
+        newDiv.setAttribute('style', 'flex:1; outline: 1px solid white; background: blue'); 
         sketchpad.appendChild(newDiv)
-        console.log("test:" + i)
     }
 }
 
+//Event listeners Clear Grid
+let btnClearGrid = document.querySelector('#btnClearGrid')
+btnClearGrid.onclick = () => clearGrid(xAxis, yAxis)
+
 // Function to clear the grid
-
 function clearGrid () {
-
+        let sketchpad = document.querySelector('#container')        
+        sketchpad.innerHTML = ''    
 }
